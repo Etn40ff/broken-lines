c24b5f52170e37441d4d7eaad432c9f63c3f35f5
Etn40ff <sstella@ncsu.edu>
2014-07-06 14:01:34 -0400

    Work done on the flight to europe

--- scattering_diagrams.py
+++ scattering_diagrams.py
@@ -17,7 +17,7 @@ class ScatteringWall(SageObject):
     def _latex_(self):
         pass
 
-    def show(self, radius=10,show_label=False):
+    def tikz(self, radius=10,show_label=False):
         output = "\\draw[color=black,line width=1pt] (0,0) -- "
         endpoint = -N(radius * vector(self.slope)/norm(vector(self.slope)))
         output += str(endpoint) + ";\n"
@@ -61,10 +61,10 @@ class ScatteringDiagram(SageObject):
         new_diagram.append(wall)
         self.walls = tuple(new_diagram)
 
-    def show(self, radius=10,show_label=False):
+    def tikz(self, radius=10,show_label=False):
         output = ""
         for W in self.walls:
-            output += W.show(radius=radius, show_label=show_label)
+            output += W.tikz(radius=radius, show_label=show_label)
         return output
 
 class ScatteringRing(SageObject):
@@ -74,56 +74,124 @@ class ScatteringRing(SageObject):
     def __init__(self, b, c):
         self._b = b
         self._c = c
-        
-        var('x,y')
-        self.scatter_ring=ZZ[x,y].fraction_field()
-        self.x = self.scatter_ring(x)
-        self.y = self.scatter_ring(y)
+        self.scatter_ring = LaurentPolynomialRing(QQ, 'x,y')
+        self.scatter_field = self.scatter_ring.fraction_field()
+        (self.x, self.y) = self.scatter_ring.gens()
     
     @cached_method
-    def scattering_diagram(self, depth):
+    def scattering_diagram(self, max_degree):
         # this map should be made recursive
         W = ScatteringWall
         x = self.x
         y = self.y
         b = self._b 
         c = self._c
-        diagram = ScatteringDiagram([ W((-1,0),1+x**b), W((0,-1),1+y**c), W((1,0),1+x**b), W((0,1),1+y**c) ])
-        for k in range(2,depth+1):
+        diagram = ScatteringDiagram([ W((0,-1),1+y**c), W((1,0),1+x**b), W((0,1),1+y**c), W((-1,0),1+x**b) ])
+        for k in range(2,max_degree+1):
            diagram = self._add_walls(diagram, k)
         return diagram
 
     def _path_product(self, scatter_diagram, k):
-        X = self.x
+        X = self.scatter_field(self.x)
         for wall in scatter_diagram:
             slope = wall.slope
             f = wall.function
             g = gcd(slope)
-            X = X(x=self.x*f**(-slope[1]/g),y=self.y*f**(slope[0]/g))
+            new_x = self.scatter_field(self.x*f**(-slope[1]/g))
+            new_y = self.scatter_field(self.y*f**(slope[0]/g))
+            X = X(x=new_x,y=new_y)
             X = taylor(X,(self.x,0),(self.y,0),k-1)
-        return X
+        return self.scatter_ring(X)
 
     def _add_walls(self, diagram, k):
         P = self._path_product(diagram, k)
         for i in range(2,k-1):
             g = gcd(i-1, k-i-1)
-            C = g*P.coefficient(self.x,i).coefficient(self.y,k-1-i)/(k-1-i)
+            C = g*P.coefficient(self.x**i*self.y**(k-1-i))/(k-1-i)
             if C != 0:
                 for j in range(2,len(diagram)):
                     wall = diagram[j]
                     slope = wall.slope
                     f = wall.function
                     if slope[0] == (i-1)/g and slope[1] == (k-1-i)/g:
-                        replacement_wall = ScatteringWall( slope, f+C*self.x**(i-1)*self.y**(k-1-i) )
+                        replacement_polynomial = f+C*self.x**(i-1)*self.y**(k-1-i)
+                        #replacement_polynomial = replacement_polynomial.polynomial(self.scatter_ring)
+                        replacement_wall = ScatteringWall( slope, replacement_polynomial )
                         diagram[j] = replacement_wall 
                         break
-                    elif slope[0] == 0 or slope[1]/slope[0] > RR(k-1-i)/(i-1):
-                        new_wall = ScatteringWall( ((i-1)/g,(k-1-i)/g), 1+C*self.x**(i-1)*self.y**(k-1-i) )
+                    elif slope[0] == 0 or (i-1)*slope[1] > (k-1-i)*slope[0]*sign(slope[0]):
+                        new_polynomial = 1+C*self.x**(i-1)*self.y**(k-1-i)
+                        #new_polynomial = new_polynomial.polynomial(self.scatter_ring)
+                        new_wall = ScatteringWall( ((i-1)/g,(k-1-i)/g), new_polynomial )
                         diagram.insert(j, new_wall)
                         break
                     else:
                         j += 1
         return diagram
+
+    @cached_method
+    def broken_lines(self, init_momentum, end_point):
+        degree = _monomial_degree(init_momentum)
+        if any( x > 0 for x in degree ):
+            return (BrokenLine(init_momentum),)
+
+        total_degree = -sum(degree)
+        diagram = self.scattering_diagram(total_degree)
+        
+        clockwise_diagram = ScatteringDiagram([ W for W in diagram if _side(W.slope,degree) == "clockwise"  ])
+        counterclockwise_diagram = ScatteringDiagram(reversed([ W for W in diagram if _side(W.slope,degree) == "counterclockwise"  ]))
+    
+        clockwise_lines = [BrokenLine(init_momentum)]
+        for wall in clockwise_diagram:
+            temp_lines = []
+            for line in clockwise_lines:
+                temp_lines.append(line)
+                last_momentum = line.monomial()
+                for new_segment in self._scatter_at_wall(last_momentum,wall):
+                    if _monomial_degree(new_segment.monomial)[1]<0 and new_segment.monomial != last_momentum:
+                        new_line=copy(line)
+                        new_line.append_segment(new_segment)
+                        temp_lines.append(new_line)
+            clockwise_lines = temp_lines
+
+        counterclockwise_lines = [BrokenLine(init_momentum)]
+        for wall in counterclockwise_diagram:
+            temp_lines = []
+            for line in counterclockwise_lines:
+                temp_lines.append(line)
+                last_momentum = line.monomial()
+                for new_segment in self._scatter_at_wall(last_momentum,wall):
+                    if _monomial_degree(new_segment.monomial)[0]<0 and new_segment.monomial != last_momentum:
+                        new_line=copy(line)
+                        new_line.append_segment(new_segment)
+                        temp_lines.append(new_line)
+            counterclockwise_lines = temp_lines
+        
+        broken_lines = []
+        for line in clockwise_lines:
+            final_direction = _monomial_degree(line.monomial())
+            if final_direction[0] > 0:
+                broken_lines.append(line)
+            elif end_point[0]*final_direction[1]-end_point[1]*final_direction[0] < 0:
+                broken_lines.append(line)
+        for line in counterclockwise_lines:
+            final_direction = _monomial_degree(line.monomial())
+            if final_direction[1] > 0:
+                broken_lines.append(line)
+            elif end_point[0]*final_direction[1]-end_point[1]*final_direction[0] > 0:
+                broken_lines.append(line)
+        
+        return tuple(broken_lines)
+    
+    def _scatter_at_wall(self, momentum, wall):
+        momentum_exp = _monomial_degree(momentum)
+        exp = abs(wall.slope[0]*momentum_exp[1]-wall.slope[1]*momentum_exp[0])
+        scattered_function = momentum*(wall.function**exp)
+        scattered_monomials = scattered_function.monomials()
+        scattered_segments = []
+        for mon in scattered_monomials:
+            scattered_segments.append(BrokenLineSegment(scattered_function.monomial_coefficient(mon), mon, wall.slope))
+        return tuple(scattered_segments)
 
 class BrokenLine(SageObject):
     """r
@@ -131,6 +199,9 @@ class BrokenLine(SageObject):
     """
     
     def __init__(self, initial_momentum):
+        if type(initial_momentum) == tuple:
+            # do something to pass integers instead of monomials
+            pass
         self.line_segments = (BrokenLineSegment(1, initial_momentum, None),)
 
     def __repr__(self):
@@ -157,6 +228,11 @@ class BrokenLine(SageObject):
     def coefficient(self):
         return prod([ x.coefficient for x in self.line_segments ])
 
+    def monomial(self):
+        return self.line_segments[-1].monomial
+
+    def tikz(self, end_point):
+        pass
 
 
 class BrokenLineSegment(SageObject):
@@ -180,32 +256,33 @@ class BrokenLineSegment(SageObject):
         output += "."
         return output
 
+###
+# helper functions
+##
+
+def _monomial_degree(monomial):
+        monomial = monomial.parent().fraction_field()(monomial)
+        return vector(monomial.numerator().exponents()[0])-vector(monomial.denominator().exponents()[0])
+
+def _side(to_check, reference):
+    if to_check == (-1,0) or to_check == (0,1):
+        return "clockwise"
+    if  to_check == (0,-1) or to_check == (1,0):
+        return "counterclockwise"
+    if reference[0]*to_check[1] > reference[1]*to_check[0]:
+        return "clockwise"
+    if reference[0]*to_check[1] < reference[1]*to_check[0]:
+        return "counterclockwise"
+    return "colinear"
+
+#################################
+# Old stuff
+################################
+
+
 #    #scattering methods
-#    def scatter_monomial(self,momentum,alpha,beta,scatter_poly=-1):
-#        #############
-#        # Compute bad lands scattering polynomials here
-#        #############
-#
-#        if scatter_poly == -1:
-#            scatter_poly = 1 + self._x^(self._b*alpha)*self._y^(self._c*beta)
-#        momentum_exp = vector(momentum.numerator().exponents()[0])-vector(momentum.denominator().exponents()[0])
-#        
-#        exp = abs(-beta*momentum_exp[0]+alpha*momentum_exp[1])
-#        scattered_poly = momentum*scatter_poly^exp
-#        #print scattered_poly
-#        denom = scattered_poly.denominator()
-#        numer = scattered_poly.numerator()
-#        
-#        scattered_monomials = []
-#        for mon in numer.monomials():
-#            scattered_monomials.append([numer.monomial_coefficient(mon),mon/denom,(alpha,beta)])
-#        
-#        return scattered_monomials
 #        
 #        
-#    def monomial_degree(self,monomial):
-#        return vector(monomial.numerator().exponents()[0])-vector(monomial.denominator().exponents()[0])
-#
 #
 #    def find_walls(self,init_momentum):
 #        counterclockwise_root_walls = [(0,1)]
@@ -217,7 +294,7 @@ class BrokenLineSegment(SageObject):
 #        check1 = true
 #        n=0
 #        while check1:
-#            root = (sqrt((sqrt(self._c/self._b)^(n%2)*chebyshev_U(n,sqrt(self._b*self._c)/2))^2),sqrt((sqrt(self._b/self._c)^((n-1)%2)*chebyshev_U(n-1,sqrt(self._b*self._c)/2))^2))
+#            root = (sqrt((sqrt(self._c/self._b)**(n%2)*chebyshev_U(n,sqrt(self._b*self._c)/2))**2),sqrt((sqrt(self._b/self._c)**((n-1)%2)*chebyshev_U(n-1,sqrt(self._b*self._c)/2))**2))
 #            check1 = false
 #            if momentum_exp[0]*root[1]-momentum_exp[1]*root[0]>0:
 #                if momentum_exp[0]+self._b*root[0]<0:
@@ -231,7 +308,7 @@ class BrokenLineSegment(SageObject):
 #        check2 = true
 #        n=0
 #        while check2:
-#            root = (sqrt((sqrt(self._c/self._b)^((n-1)%2)*chebyshev_U(n-1,sqrt(self._b*self._c)/2))^2),sqrt((sqrt(self._b/self._c)^(n%2)*chebyshev_U(n,sqrt(self._b*self._c)/2))^2))
+#            root = (sqrt((sqrt(self._c/self._b)**((n-1)%2)*chebyshev_U(n-1,sqrt(self._b*self._c)/2))**2),sqrt((sqrt(self._b/self._c)**(n%2)*chebyshev_U(n,sqrt(self._b*self._c)/2))**2))
 #            check2 = false
 #            if momentum_exp[0]*root[1]-momentum_exp[1]*root[0]<0:
 #                if momentum_exp[1]+self._c*root[1]<0:
@@ -257,46 +334,6 @@ class BrokenLineSegment(SageObject):
 #        return [clockwise_walls,counterclockwise_walls]
 #        
 #        
-#    def broken_lines(self,init_momentum,Q):
-#        (clockwise_walls,counterclockwise_walls) = self.find_walls(init_momentum)
-#
-#        clockwise_lines = [[[1,init_momentum,(0,0)]]]
-#        for wall in clockwise_walls:
-#            temp_lines = []
-#            for line in clockwise_lines:
-#                temp_lines.append(line)
-#                last_momentum = line[-1]
-#                for momentum_pair in self.scatter_monomial(last_momentum[1],wall[0],wall[1]):
-#                    if momentum_pair[1].denominator().exponents()[0][1]>0 and momentum_pair[1] != last_momentum[1]:
-#                        temp_lines.append(copy(line)+[momentum_pair])
-#            clockwise_lines = temp_lines
-#
-#        counterclockwise_lines = [[[1,init_momentum,(0,0)]]]
-#        for wall in counterclockwise_walls:
-#            temp_lines = []
-#            for line in counterclockwise_lines:
-#                temp_lines.append(line)
-#                last_momentum = line[-1]
-#                for momentum_pair in self.scatter_monomial(last_momentum[1],wall[0],wall[1]):
-#                    if momentum_pair[1].denominator().exponents()[0][0]>0 and momentum_pair[1] != last_momentum[1]:
-#                        temp_lines.append(copy(line)+[momentum_pair])
-#            counterclockwise_lines = temp_lines
-#        
-#        broken_lines = []
-#        for line in clockwise_lines:
-#            final_direction = self.monomial_degree(line[-1][1])
-#            if final_direction[0] > 0:
-#                broken_lines.append(line)
-#            elif Q[0]*final_direction[1]-Q[1]*final_direction[0] < 0:
-#                broken_lines.append(line)
-#        for line in counterclockwise_lines:
-#            final_direction = self.monomial_degree(line[-1][1])
-#            if final_direction[1] > 0:
-#                broken_lines.append(line)
-#            elif Q[0]*final_direction[1]-Q[1]*final_direction[0] > 0:
-#                broken_lines.append(line)
-#        
-#        return broken_lines
 #
 #
 #    def GreedyElementBrokenLine(self,a1,a2,Q):
@@ -306,7 +343,7 @@ class BrokenLineSegment(SageObject):
 #        """
 #
 #        output = 0
-#        for line in self.broken_lines(self._x^(-a1)*self._y^(-a2),Q):
+#        for line in self.broken_lines(self._x**(-a1)*self._y**(-a2),Q):
 #            coeff = prod([i[0] for i in line])
 #            output += coeff*line[-1][1]
 #        return output 
@@ -340,10 +377,10 @@ class BrokenLineSegment(SageObject):
 #
 #    def draw_walls(self,a1,a2,Q):
 #        output = ""
-#        walls = self.find_walls(self._x^(-a1)*self._y^(-a2))
-#        length = ceil(sqrt(Q[0]^2+Q[1]^2))
+#        walls = self.find_walls(self._x**(-a1)*self._y**(-a2))
+#        length = ceil(sqrt(Q[0]**2+Q[1]**2))
 #        for w in walls[0][:-1]+walls[1][:-1]:
-#            wall_length = ceil(sqrt(w[0]^2+w[1]^2))
+#            wall_length = ceil(sqrt(w[0]**2+w[1]**2))
 #            output += "  \\draw[color=black,line width=1pt] (0,0) -- ("
 #            output += str(-ceil(self._b*length*w[0]/wall_length))+","+str(-ceil(self._c*length*w[1]/wall_length))+");\n"
 #        for w in [(0,1),(1,0)]:
@@ -354,7 +391,7 @@ class BrokenLineSegment(SageObject):
 #        
 #        
 #    def draw_broken_lines(self,a1,a2,Q):
-#        #creates tikz pictures of all broken lines with initial momentum x^{-a1}y^{-a2} ending at Q
+#        #creates tikz pictures of all broken lines with initial momentum x**{-a1}y**{-a2} ending at Q
 #        working_dir="/tmp/"
 #        #filename = "broken_lines"+str(self._b)+str(self._c)+"-("+str(a1)+","+str(a2)+")"#-("+str(Q[0])+","+str(Q[1])+")"
 #        filename = "sage_output"
@@ -371,7 +408,7 @@ class BrokenLineSegment(SageObject):
 #        if a1 != 0:
 #            TeXFile.write("x")
 #            if a1 != -1:
-#                TeXFile.write("^{")
+#                TeXFile.write("**{")
 #                if a1 > 0:
 #                    TeXFile.write("-")
 #                    TeXFile.write(str(a1)+"}")
@@ -380,7 +417,7 @@ class BrokenLineSegment(SageObject):
 #        if a2 != 0:
 #            TeXFile.write("y")
 #            if a2 != -1:
-#                TeXFile.write("^{")
+#                TeXFile.write("**{")
 #                if a2 > 0:
 #                    TeXFile.write("-")
 #                    TeXFile.write(str(a2)+"}")
@@ -392,7 +429,7 @@ class BrokenLineSegment(SageObject):
 #        TeXFile.write(" \\begin{tikzpicture}\n")
 #        TeXFile.write(self.draw_walls(a1,a2,Q))
 #        
-#        for line in self.broken_lines(self._x^(-a1)*self._y^(-a2),Q):
+#        for line in self.broken_lines(self._x**(-a1)*self._y**(-a2),Q):
 #            TeXFile.write(self.draw_broken_line(line,Q))
 #
 #
@@ -405,40 +442,3 @@ class BrokenLineSegment(SageObject):
 #        
 #        import subprocess
 #        subprocess.call(['pdflatex', '-halt-on-error', filename], cwd=working_dir, stdout=subprocess.PIPE)
-#
-#    def _thetad(self, scatter_diagram, k):
-#        X = self._x
-#        for (slope,f) in scatter_diagram:
-#            g = gcd(slope)
-#            X = X(x=self._x*f^(-slope[1]/g),y=self._y*f^(slope[0]/g))
-#            X = taylor(X,(self._x,0),(self._y,0),k-1)
-#        return X
-#
-#    def _scatter(self, scatter_diagram, k):
-#        P = self._thetad(scatter_diagram, k)
-#        scatter_diagram = list(scatter_diagram)
-#        for i in range(2,k-1):
-#            g = gcd(i-1, k-i-1)
-#            C = g*P.coefficient(self._x,i).coefficient(self._y,k-1-i)/(k-1-i)
-#            for j in range(2,len(scatter_diagram)):
-#                (slope,f) = scatter_diagram[j]
-#                if C == 0:
-#                    break
-#                if slope[0] == (i-1)/g and slope[1] == (k-1-i)/g:
-#                    new_ray = (slope, f+C*self._x^(i-1)*self._y^(k-1-i))
-#                    C = 0
-#                    scatter_diagram[j] = new_ray 
-#                elif slope[0] == 0 or slope[1]/slope[0] >= (k-1-i)/(i-1):
-#                    new_ray = (((i-1)/g,(k-1-i)/g),1+C*self._x^(i-1)*self._y^(k-1-i))
-#                    scatter_diagram.insert(j, new_ray)
-#                    C = 0
-#                else:
-#                    j += 1
-#        return tuple(scatter_diagram)
-#
-#    def _get_scattering_diagram(self,fx, fy, ktop):
-#        scattering_diagram = (((-1,0),fx),((0,-1),fy),((1,0),fx),((0,1),fy))
-#        for k in range(2,ktop+1):
-#           scattering_diagram = self._scatter(scattering_diagram,k)
-#        return scattering_diagram
-#
